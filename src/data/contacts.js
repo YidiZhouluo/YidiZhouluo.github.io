0a8b783875c67b3a9c38d26594d0e2edaa97d4e7
zlyd <zlyd20060615@163.com>
2026-06-10 21:36:16 +0800

    完善联系方式页面

---
+++ src/data/contacts.js
@@ -0,0 +1,84 @@
+export const contactGroups = [
+  {
+    title: "邮箱",
+    items: [
+      {
+        label: "个人邮箱",
+        value: "zhoulyd@126.com",
+        href: "mailto:zhoulyd@126.com"
+      },
+      {
+        label: "Gmail",
+        value: "zhouluoyidi@gmail.com",
+        href: "mailto:zhouluoyidi@gmail.com"
+      },
+      {
+        label: "教育邮箱",
+        value: "4124530079@email.sdfmu.edu.cn",
+        href: "mailto:4124530079@email.sdfmu.edu.cn",
+        note: "教育邮箱"
+      }
+    ]
+  },
+  {
+    title: "学术主页",
+    items: [
+      {
+        label: "Google Scholar",
+        value: "Google Scholar",
+        href: "https://scholar.google.com/citations?user=R4WXdXMAAAAJ&hl=zh-CN"
+      },
+      {
+        label: "Semantic Scholar",
+        value: "Yidi Zhouluo",
+        href: "https://www.semanticscholar.org/author/Yidi-Zhouluo/2275024698"
+      },
+      {
+        label: "AMiner",
+        value: "AMiner Profile",
+        href: "https://www.aminer.cn/profile/68f0ec1d9329f86005abb67c"
+      },
+      {
+        label: "ORCID",
+        value: "0009-0008-2044-115X",
+        href: "https://orcid.org/0009-0008-2044-115X"
+      },
+      {
+        label: "ResearchGate",
+        value: "暂未开通"
+      }
+    ]
+  },
+  {
+    title: "代码平台",
+    items: [
+      {
+        label: "GitHub",
+        value: "YidiZhouluo",
+        href: "https://github.com/YidiZhouluo"
+      }
+    ]
+  },
+  {
+    title: "社交平台与即时联系",
+    items: [
+      {
+        label: "Bilibili",
+        value: "Yidi-Zhouluo"
+      },
+      {
+        label: "小红书",
+        value: "Yidi Zhouluo",
+        note: "小红书号：zlyd_CV"
+      },
+      {
+        label: "微信",
+        value: "zlyd-CV"
+      },
+      {
+        label: "X / Twitter",
+        value: "保密嘿嘿"
+      }
+    ]
+  }
+];
